ff2cbbd5e19760b7f8bb0c4e2c298590c1c95fa9
rafaelcoppola <rafael-coppola@hotmail.com>
2021-05-13 23:46:12 -0300

    Keep bank 1.03

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 //slider
-$('.carousel').slick({
-    dots: true,
+/*$('.feedback-carousel').slick({
+    dots: false,
     arrows: true,
     infinite: true,
     speed: 500,
@@ -10,7 +10,49 @@ $('.carousel').slick({
     autoplayspeed: 2000,
     centerMode: true,
 });
-$('.carousel').slick()
+$('.feedback-carousel').slick()*/
 
 //animation WOW
 new WOW().init();
+
+$('.feedback-carousel').slick({
+    dots: false,
+    arrows: true,
+    infinite: true,
+    fade: true,
+    autoplay: true,
+    autoplayspeed: 2300,
+    speed: 300,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    adaptiveHeight: false,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          infinite: true,
+          dots: false
+        }
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+      // You can unslick at a given breakpoint now by adding:
+      // settings: "unslick"
+      // instead of a settings object
+    ]
+  });
+  $('.feedback-carousel').slick()
